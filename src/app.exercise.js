aa8db437b64ce4f3cf31ee03e5d10bdca9590105
Julián Scialabba <julian.scialabba@gmail.com>
2020-10-26 17:55:26 -0300

    Add Exercise 04

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -1,28 +1,57 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core'
+import { jsx } from '@emotion/core'
 
 import React from 'react'
-// 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
-import {AuthenticatedApp} from './authenticated-app'
-import {UnauthenticatedApp} from './unauthenticated-app'
+import * as auth from 'auth-provider'
+import { client } from './utils/api-client'
 
-function App() {
-  // 🐨 useState for the user
+import { AuthenticatedApp } from './authenticated-app'
+import { UnauthenticatedApp } from './unauthenticated-app'
+import { useAsync } from './utils/hooks';
+import { FullPageSpinner } from 'components/lib'
+import * as colors from './styles/colors';
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
-
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
-
-  // 🐨 if there's a user, then render then AuthenitcatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
-
-  return <UnauthenticatedApp />
+async function getUser() {
+  let user = null;
+  const token = await auth.getToken();
+  if (token) {
+    const data = await client('me', token);
+    user = data && data.user;
+  }
+  return user;
 }
 
-export {App}
+function App() {
+  const { data, error, isIdle, isLoading, isError, setData, run } = useAsync()
+  const login = form => auth.login(form).then(u => setData(u))
+  const register = form => auth.register(form).then(u => setData(u))
+  const logout = () => {
+    auth.logout();
+    setData(null);
+  }
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  if (isIdle || isLoading) return <FullPageSpinner />
+  if (isError) return (<div
+    css={{
+      color: colors.danger,
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <p>Uh oh... There's a problem. Try refreshing the app.</p>
+    <pre>{error.message}</pre>
+  </div>)
+  return data ? <AuthenticatedApp user={data} logout={logout} /> : <UnauthenticatedApp login={login} register={register} />
+}
+
+export { App }
 
 /*
 eslint
